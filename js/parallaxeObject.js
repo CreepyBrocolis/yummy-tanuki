290eab10d07d9ecbb6a8542c2db5a26799d73c6e
Babouchot <bastien.maureille@hotmail.fr>
2015-08-22 20:16:20 +0200

    fixed parallaxe with camera movements

--- js/parallaxeObject.js
+++ js/parallaxeObject.js
@@ -1,22 +1,29 @@
 function ParallaxeObject(container, width, sprite, position, movementSpeed) {
   var sprite1 = new createjs.Bitmap(sprite);
-  sprite1.y = position;
+  sprite1.yPosition = position;
+  sprite1.y = sprite1.yPosition + camera.yPosition;
+  sprite1.xPosition = 0;
+  sprite1.x = sprite1.xPosition + camera.xPosition;
 
   var sprite2 = sprite1.clone();
-  sprite2.x = width;
+  sprite2.xPosition = width;
+  sprite2.x = sprite2.xPosition + camera.xPosition;
+  sprite2.yPosition = position;
+  sprite2.y = sprite2.yPosition + camera.yPosition;
 
   console.log(sprite2);
 
   container.addChild(sprite1, sprite2);
 
   function move(sprite, deltaS) {
-    sprite.x = sprite.x - deltaS * movementSpeed;
-    if (sprite.x + sprite.image.width <= 0) {
-      sprite.x = width;
+    sprite.xPosition = sprite.xPosition - deltaS * movementSpeed;
+    if (sprite.xPosition + sprite.image.width <= 0) {
+      sprite.xPosition = width;
     }
 
-    sprite.x = sprite.x + camera.xPosition;
-    sprite.y = sprite.y + camera.yPosition;
+    sprite.x = sprite.xPosition + camera.xPosition;
+    sprite.y = sprite.yPosition + camera.yPosition * (movementSpeed/100);
+
   }
 
   var tick = function (deltaS) {
